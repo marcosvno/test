07935be06b760feec0cd9791130ffd12f76b74b9
jmoana <jhenycruz7@gmail.com>
2021-12-13 20:05:38 -0300

    commit

--- application/controllers/api/Fornecedor.php
+++ application/controllers/api/Fornecedor.php
@@ -12,18 +12,41 @@ class Fornecedor extends RestController{
     }
 
     public function index_get($id=NULL){
-    
+        if ($id==NULL){
+            $resultado = $this->FornecedorModel->recuperarTodos();
+            return $this->response($resultado, RestController::HTTP_OK);
+        }else{
+            $resultado = $this->FornecedorModel->recuperarPorId($id);
+            if (count($resultado)==0){
+                $resultado["status"]=404;
+                $resultado["message"]="Id inválido";
+                return $this->response($resultado, RestController::HTTP_NOT_FOUND);
+            }else{
+                return $this->response($resultado, RestController::HTTP_OK);
+            }
+        }  
     }
 
     public function index_post(){
-        
+        $fornecedor = $this->input->post("fornecedor");
+        $this->FornecedorModel->inserir($fornecedor);
+        $resultado["status"]=true;
+        $resultado["mensagem"]="Dados inseridos com sucesso!";
+        $this->response($resultado, RestController::HTTP_OK);
     }
 
     public function index_put($id){
-        
+        $fornecedor = $this->put("fornecedor");
+        $this->FornecedorModel->atualizar($id, $fornecedor);
+        $resultado["status"]=true;
+        $resultado["messagem"]="Dados atualizados com sucesso";
+        $this->response($resultado, RestController::HTTP_OK); 
     }
 
     public function index_delete($id){
-       
+        $this->FornecedorModel->excluir($id);
+        $resultado["status"]=true;
+        $resultado["mensagem"]="Dados excluídos com sucesso";
+        $this->response($resultado, RestController::HTTP_OK);
     }
 }
--- application/controllers/api/Grupo.php
+++ application/controllers/api/Grupo.php
@@ -12,18 +12,41 @@ class Grupo extends RestController{
     }
 
     public function index_get($id=NULL){
-    
+        if ($id==NULL){
+            $resultado = $this->GrupoModel->recuperarTodos();
+            return $this->response($resultado, RestController::HTTP_OK);
+        }else{
+            $resultado = $this->GrupoModel->recuperarPorId($id);
+            if (count($resultado)==0){
+                $resultado["status"]=404;
+                $resultado["message"]="Id inválido";
+                return $this->response($resultado, RestController::HTTP_NOT_FOUND);
+            }else{
+                return $this->response($resultado, RestController::HTTP_OK);
+            }
+        }  
     }
 
     public function index_post(){
-        
+        $grupo = $this->input->post("grupo");
+        $this->GrupoModel->inserir($grupo);
+        $resultado["status"]=true;
+        $resultado["mensagem"]="Dados inseridos com sucesso!";
+        $this->response($resultado, RestController::HTTP_OK);
     }
 
     public function index_put($id){
-        
+        $grupo = $this->put("grupo");
+        $this->GrupoModel->atualizar($id, $grupo);
+        $resultado["status"]=true;
+        $resultado["messagem"]="Dados atualizados com sucesso";
+        $this->response($resultado, RestController::HTTP_OK); 
     }
 
     public function index_delete($id){
-       
+        $this->GrupoModel->excluir($id);
+        $resultado["status"]=true;
+        $resultado["mensagem"]="Dados excluídos com sucesso";
+        $this->response($resultado, RestController::HTTP_OK);
     }
 }
